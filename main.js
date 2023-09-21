01fc7250a92924b6513107d9fc7689c6528ca47b
joby foster <jobyjfoster@gmail.com>
2023-09-21 11:13:26 -0500

    added saving advice

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 const request = new XMLHttpRequest();
 const url = 'https://api.adviceslip.com/advice';
 const refreshButton = document.getElementById('refresh-button');
+const saveButton = document.getElementById('save-button');
 const adviceBox = document.getElementById('advice-box');
-const errorMsg = document.getElementById('error-msg')
+const adviceText = document.getElementById('advice-text');
+const errorMsg = document.getElementById('error-msg');
+
+const savedAdviceBox = document.getElementById('saved-advice-box');
+
+let savedAdvice = [];
 
 let canMakeRequest = true;
-
-let adviceText = "";
 
 function sendRequest() {
     if(canMakeRequest) {
@@ -18,7 +22,28 @@ function sendRequest() {
             canMakeRequest = true;
         }, 2000);
     } else {
-        errorMsg.innerHTML = "Requesting too fast... please slow down"
+        errorMsg.innerHTML = "Requesting too fast... please slow down!"
+    }
+    
+}
+
+function saveAdvice() {
+    let adviceString;
+    let newAdvice;
+    
+    adviceString = JSON.parse(request.responseText).slip.advice;
+    if (!savedAdvice.includes(adviceString)){
+        savedAdvice.push(adviceString);
+        
+        newAdvice = document.createElement("div");
+        newAdvice.classList.add("saved-advice");
+        newAdvice.innerHTML = adviceString;
+
+        errorMsg.innerHTML = "";
+
+        savedAdviceBox.appendChild(newAdvice);
+    } else {
+        errorMsg.innerHTML = "Advice already saved. Please refresh and try again."
     }
     
 }
@@ -35,3 +60,4 @@ window.onload = function () {
 }
 
 refreshButton.addEventListener("click", sendRequest);
+saveButton.addEventListener("click", saveAdvice)
